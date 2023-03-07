321e39b9cdec569f6c1b95bd98940790b40f8256
alessiam1909 <ale.menon.vr@gmail.com>
2023-03-07 17:17:45 +0100

    Aggiunta dei generi nella create tramite select e aggiunta in index, show

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +10,10 @@ class Movie extends Model
     protected $fillable = ['title', 'original_title', 'nationality', 'release_date', 'vote', 'cast', 'cover_path'];
     
     use HasFactory;
+
+    
+    public function genre(): BelongsTo{
+        return $this->belongsTo(Genre::class);
+    }
 }
+
